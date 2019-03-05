5553c755ef21bdd4760705493ac029d5de7d4ac2
Spencer Hewitt <spencerhewitt@dhcp-38-247.mil.secure.int.wireless.unca.edu>
2019-03-05 12:11:42 -0500

    testing playing an audio file

--- bot.js
+++ bot.js
@@ -1,49 +1,59 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+//TODO:
+//Make/add icons to bot
+//Music integration
+//Image manipulation
+//
+
+var Discord = require("discord.io");
+var logger = require("winston");
+var auth = require("./auth.json");
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+logger.add(new logger.transports.Console(), {
+  colorize: true
 });
-logger.level = 'debug';
-// Initialize Discord Bot
+logger.level = "debug";
+// Initialize Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+  token: auth.token,
+  autorun: true
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on("ready", function(evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '.') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
+bot.on("message", function(user, userID, channelID, message, evt) {
+  //listens for a command starting with '.'
+  if (message.substring(0, 1) == ".") {
+    var args = message.substring(1).split(" ");
+    var cmd = args[0];
 
-            
-            // Just add any case commands if you want to..
-         }
-         switch(cmd) {
-             case 'cream':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'cream'
-                });
-                break;
-         }
-     }
-});
+    args = args.splice(1);
+
+    //default test case .ping
+    switch (cmd) {
+      case "ping":
+        bot.sendMessage({
+          to: channelID,
+          message: "Pong!"
+        });
+        break;
+
+    }
+    switch (cmd) {
+      case "cream":
+        bot.sendMessage({
+          to: channelID,
+          message: "cream"
+        });
+        break;
+    }
+    switch(cmd) {
+        case "numbers":
+        client.playSound("/assets/media/test.mp3");
+        break;
+    }
+    }
+  }
+,);
