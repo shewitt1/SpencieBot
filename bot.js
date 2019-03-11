e49807ffbf99d6b9f8f8ccde01f5c9b04b3b578c
Spencer Hewitt <spencerhewitt@peenpirate.attlocal.net>
2019-03-11 05:00:42 -0400

    added another gnome command to test streaming from youtube link

--- bot.js
+++ bot.js
@@ -2,65 +2,75 @@
 const Discord = require('discord.js');
 const auth = require('./auth');
 const { Client, Attachment } = require('discord.js');
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+
+
 client = new Client()
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  switch(msg.content){
-	case '$ping':
-		msg.reply('pong');
-		break;
+
+
+
+client.on('message', message => {
+  if (message == '$ping') {
+    message.reply('pong!');
+
   }
 });
 
-client.on("message", message => {
-  const channel = message.member.voiceChannel;
 
+
+
+
+client.on("message", message => {
+const channel = message.member.voiceChannel;
 if (message == '$join') {
     if (!channel) return console.error("The channel does not exist!");
     channel.join().then(connection => {
-      // Yay, it worked!
       console.log("Successfully connected.");
     }).catch(e => {
-      // Oh no, it errored! Let's log it to console :)
       console.error(e);
     });
   }
 
-  if (message == '$disconnect') {
-    channel.destroy().then(connection => {
-        console.log('Disconnected')
+});
 
-    }).catch(e => {
-      console.error(e);
-    });
-    }
-    
 
+
+
+client.on("message", message => {
+const channel = message.member.voiceChannel;
+
+if (message == '$gnome') {
+channel.join()
+  .then(connection => {
+    const stream = ytdl('https://www.youtube.com/watch?v=z-zxaKQfW6s', { filter : 'audioonly' });
+    const dispatcher = connection.playStream(stream, streamOptions);
+    const attachment = new Attachment('https://i.ytimg.com/vi/z-zxaKQfW6s/maxresdefault.jpg');
+      
+    message.channel.send(attachment);
+  })
+  .catch(console.error);
+}
+
+});
+
+
+
+
+
+client.on("message", message => {
+const channel = message.member.voiceChannel;
+if (message == '$disconnect') {
+channel.disconnect();
+
+}
   });
-  
-
-  
 
 
-/*client.on("message", message => {
-  const channel = message.member.voiceChannel;
-  voiceChannel.disconnect()
-});
-
-/* client.on('message', message => {
-  
-  if (message.content === 'it is time to stop') {
-      
-      const attachment = new Attachment('https://i.kym-cdn.com/entries/icons/original/000/019/785/stopit.png');
-      
-      message.channel.send(attachment);
-  }
-});
-
-*/
  
  
 client.login(auth.token);
